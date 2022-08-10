a88d1c85db20404d4cd6b068828b12e7422a391e
Yilmaz <yilmaztasdelen1994@gmail.com>
2022-08-10 15:32:47 +0300

    .

--- server.js
+++ server.js
@@ -1,32 +1,46 @@
 const express = require('express');
-const { buildSchema } = require('graphql');
+//const { buildSchema } = require('graphql');
 const { graphqlHTTP } = require('express-graphql');
 
-const schema = buildSchema(`
-  type Query {
-    products: [Product]
-    orders: [Order]
-  }
-  type Product {
-    id: ID!
-    description: String!
-    reviews: [Review]
-    price: Float!
-  }
-  type Review {
-    rating: Int!
-    comment: String
-  }
-  type Order {
-    date: String!
-    subtotal: Float!
-    items: [OrderItem]
-  }
-  type OrderItem {
-    product: Product!
-    quantity: Int!
-  }
-`);
+
+const { makeExecutableSchema } = require('@graphql-tools/schema');
+
+const schemaText = `
+type Query {
+  products: [Product]
+  orders: [Order]
+}
+type Product {
+  id: ID!
+  description: String!
+  reviews: [Review]
+  price: Float!
+}
+type Review {
+  rating: Int!
+  comment: String
+}
+type Order {
+  date: String!
+  subtotal: Float!
+  items: [OrderItem]
+}
+type OrderItem {
+  product: Product!
+  quantity: Int!
+}
+`;
+/*
+we use this over buildSchema now because 
+makeExecutableSchema aproach benefit is that 
+we  are able to split our schema to smaller parts.
+*/
+const schema = makeExecutableSchema({
+
+    typeDefs: [schemaText]
+})
+
+
 
 const root = {
     products: [
