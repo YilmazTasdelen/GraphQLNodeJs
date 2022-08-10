05c2b52048b835225e628b991e71355e90f5eb77
Yilmaz <yilmaztasdelen1994@gmail.com>
2022-08-10 18:59:24 +0300

    get product by id query  added.

--- products/products.model.js
+++ products/products.model.js
@@ -21,7 +21,13 @@ function getProductsByPrice(min, max) {
     });
 }
 
+function getProductById(id) {
+    return products.find((product) => {
+        return product.id === id;
+    });
+}
 module.exports = {
     getAllProducts,
     getProductsByPrice,
+    getProductById,
 }; 
--- products/products.resolvers.js
+++ products/products.resolvers.js
@@ -7,6 +7,86 @@ module.exports = {
         },
         productsByPrice: (_, args) => { // if dont use previous arguments named as a _
             return productsModel.getProductsByPrice(args.min, args.max);
+        },
+        product: (_, args) => {
+            return productsModel.getProductById(args.id);
         }
     }
-}; 
+};
+
+/**
+ * ecample queries 
+ * 1-
+ * ------------------------------
+ * {
+  productsByPrice(min: 10, max: 50) {
+    description
+    price
+  }
+  orders {
+    subtotal
+    items {
+      quantity
+      product {
+        id
+        price
+        reviews {
+          rating
+          comment
+        }
+      }
+    }
+  }
+}
+2-
+--------------------------------------------
+{
+  products {
+    description
+  }
+  orders {
+    subtotal
+    items {
+      quantity
+      product {
+        id
+        price
+        reviews {
+          rating
+          comment
+        }
+      }
+    }
+  }
+}
+3-
+-----------------------------------------------
+   products(id:"bluejean") {
+    description
+    reviews{
+        comments
+        rating
+    }
+  }
+productsByPrice(min: 10, max: 50) {
+    description
+    price
+  }
+  orders {
+    subtotal
+    items {
+      quantity
+      product {
+        id
+        price
+        reviews {
+          rating
+          comment
+        }
+      }
+    }
+  }
+}
+ * 
+ * 
+ */
