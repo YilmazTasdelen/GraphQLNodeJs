4b75980c1a807efbc78517705b9feb2824a09402
Yilmaz <yilmaztasdelen1994@gmail.com>
2022-08-10 18:52:55 +0300

    filter products by price query added.

--- products/products.resolvers.js
+++ products/products.resolvers.js
@@ -4,6 +4,9 @@ module.exports = {
     Query: {
         products: () => {
             return productsModel.getAllProducts();
+        },
+        productsByPrice: (_, args) => { // if dont use previous arguments named as a _
+            return productsModel.getProductsByPrice(args.min, args.max);
         }
     }
 }; 
